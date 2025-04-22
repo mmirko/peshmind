6f08af2e604a7798d31cc44fb8e6d5b8c559b690
Mirko Mariotti <mirko@mirkomariotti.it>
2025-04-22 23:15:51 +0200

    :recycle: (list) renamed

--- cmd/list.go
+++ cmd/list.go
@@ -9,31 +9,37 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// listSwitchesCmd represents the listSwitches command
-var listSwitchesCmd = &cobra.Command{
-	Use:   "listSwitches",
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
 	Short: "List the switches in the database",
 	Long:  `List the switches in the database`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(c.Switches) != 0 {
 			fmt.Println("Switches Database:")
-			for switchName, ip := range c.Switches {
-				fmt.Printf("\t%s, IP: %s\n", switchName, ip)
+			for switchName, sw := range c.Switches {
+				fmt.Printf("\t%s\n", switchName)
+				if sw.Description != "" {
+					fmt.Printf("\t\tDescription: %s\n", sw.Description)
+				}
+				fmt.Printf("\t\tIP: %s\n", sw.IP)
+				fmt.Printf("\t\tUsername: %s\n", sw.Username)
+				fmt.Printf("\t\tPassword: %s\n", sw.Password)
 			}
 		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(listSwitchesCmd)
+	rootCmd.AddCommand(listCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// listSwitchesCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// listSwitchesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
