f73c3e2e17946ab1a3c1e45a65e35a908e62fb3a
Mirko Mariotti <mirko@mirkomariotti.it>
2025-04-22 18:03:35 +0200

    :sparkles: (rules) direct connection

--- kbpool/rules.pl
+++ kbpool/rules.pl
@@ -5,3 +5,10 @@ far(X,Y) :-	seen(X,K,MIDPORTX),
 		switch(K), switch(X), switch(Y), X \= Y, Y \= K, X \= K, 
 		PORTX == MIDPORTX,
 		PORTY == MIDPORTY.
+
+direct(X,Y) :-	seen(X,K,MIDPORTX),
+		seen(Y,K,MIDPORTY),
+		seen(X,Y,PORTX),
+		seen(Y,X,PORTY),
+		switch(K), switch(X), switch(Y), X \= Y, Y \= K, X \= K,
+		(PORTX \= MIDPORTX ; PORTY \= MIDPORTY).	
