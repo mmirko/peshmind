f1a4223351e35f81ef04858ebb97f7aefe4c186f
Mirko Mariotti <mirko@mirkomariotti.it>
2025-04-23 11:26:50 +0200

    :sparkles: (rules) direct port detection

--- kbpool/rules.pl
+++ kbpool/rules.pl
@@ -13,4 +13,10 @@ direct(X,Y) :-	seen(X,K,MIDPORTX),
 		switch(K), switch(X), switch(Y), X \= Y, Y \= K, X \= K,
 		(PORTX \= MIDPORTX ; PORTY \= MIDPORTY).
 
-	
+directp(X,Y,PORTX,PORTY) :- seen(X,K,MIDPORTX),
+		seen(Y,K,MIDPORTY),
+		seen(X,Y,PORTX),
+		seen(Y,X,PORTY),
+		switch(K), switch(X), switch(Y), X \= Y, Y \= K, X \= K,
+		(PORTX \= MIDPORTX ; PORTY \= MIDPORTY).
+
