7de9eaee27bda11a4befba9f950e3de39bb1c637
Mirko Mariotti <mirko@mirkomariotti.it>
2026-03-20 09:37:55 +0100

    :sparkles: (update) group update

--- cmd/update.go
+++ cmd/update.go
@@ -18,16 +18,24 @@ var updateCmd = &cobra.Command{
 	Long:  `Update the database of a switch`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if switchName == "" {
-			fmt.Println("Switch name is required")
+			fmt.Println("Switch name or \"all\" is required")
 			return
 		}
 
-		if _, ok := c.Switches[switchName]; !ok {
-			fmt.Printf("Switch %s not found\n", switchName)
-			return
-		}
+		swToUpdate := make([]string, 0)
 
-		sw := c.Switches[switchName]
+		if switchName != "all" {
+			if _, ok := c.Switches[switchName]; !ok {
+				fmt.Printf("Switch %s not found\n", switchName)
+				return
+			} else {
+				swToUpdate = append(swToUpdate, switchName)
+			}
+		} else {
+			for name := range c.Switches {
+				swToUpdate = append(swToUpdate, name)
+			}
+		}
 
 		fmt.Print("Enter admin password: ")
 		password, err := term.ReadPassword(int(syscall.Stdin))
@@ -40,25 +48,40 @@ var updateCmd = &cobra.Command{
 
 		// Convert to string if needed
 		passwordStr := string(password)
-		fmt.Println("Password entered:", passwordStr)
-		sw.Password = passwordStr
+		// fmt.Println("Password entered:", passwordStr)
 
-		if err := sw.ApplyTemplate(); err != nil {
-			fmt.Println("Failed to apply template:")
-			fmt.Println(err.Error())
-			return
-		}
+		for _, switchName := range swToUpdate {
 
-		if err := sw.FetchData(); err != nil {
-			fmt.Println("Failed to fetch data:")
-			fmt.Println(err.Error())
-			return
-		}
+			sw := c.Switches[switchName]
+			if c.Switches[switchName].Password == "ask" {
+				sw.Password = passwordStr
+			} else {
+				sw.Password = c.Switches[switchName].Password
+			}
 
-		if err := sw.SaveKB(kbPool); err != nil {
-			fmt.Println("Failed to save KB:")
-			fmt.Println(err.Error())
-			return
+			if err := sw.ApplyTemplate(); err != nil {
+				fmt.Println("Failed to apply template:")
+				fmt.Println(err.Error())
+				return
+			} else {
+				fmt.Printf("Template applied successfully for switch %s\n", switchName)
+			}
+
+			if err := sw.FetchData(); err != nil {
+				fmt.Println("Failed to fetch data:")
+				fmt.Println(err.Error())
+				return
+			} else {
+				fmt.Printf("Data fetched successfully for switch %s\n", switchName)
+			}
+
+			if err := sw.SaveKB(kbPool); err != nil {
+				fmt.Println("Failed to save KB:")
+				fmt.Println(err.Error())
+				return
+			} else {
+				fmt.Printf("KB saved successfully for switch %s\n", switchName)
+			}
 		}
 	},
 }
